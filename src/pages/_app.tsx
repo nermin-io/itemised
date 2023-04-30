1b313d0873fc8dde9ae1b0f831544b71174b3272
Nermin <sehicnermin97@gmail.com>
2023-04-30 15:45:52 +1000

    remove next.js font optimization.. tbc

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,17 +1,14 @@
 import "@/styles/globals.scss";
 import type { AppProps } from "next/app";
-import { inter } from "@/fonts";
 import Layout from "@/components/Layout";
 import TodoProvider from "@/providers/TodoProvider";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <div className={inter.className}>
-      <TodoProvider>
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
-      </TodoProvider>
-    </div>
+    <TodoProvider>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </TodoProvider>
   );
 }
