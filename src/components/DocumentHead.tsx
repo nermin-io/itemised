5a57503199158c12f9ec57db26622c8ff0c59dec
Nermin <sehicnermin97@gmail.com>
2023-05-06 17:26:41 +1000

    add open graph and favicons

---
+++ src/components/DocumentHead.tsx
@@ -0,0 +1,48 @@
+import Head from "next/head";
+import React from "react";
+
+interface Props {
+  title: string;
+  description: string;
+}
+
+const DocumentHead: React.FC<Props> = ({ title, description }) => {
+  return (
+    <Head>
+      <title>{title}</title>
+      <meta name="description" content={description} />
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/apple-touch-icon.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/favicon-32x32.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/favicon-16x16.png"
+      />
+      <link rel="manifest" href="/site.webmanifest" />
+      <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#222222" />
+      <meta name="msapplication-TileColor" content="#da532c" />
+      <meta name="theme-color" content="#222222" />
+      <meta property="og:image" content="/images/ogimage.png" />
+      <meta property="og:image:type" content="image/png" />
+      <meta property="og:image:width" content="1200" />
+      <meta property="og:image:height" content="630" />
+      <meta property="og:image:alt" content="Itemised Logo" />
+      <meta property="og:type" content="website" />
+      <meta property="og:url" content="https://itemised.co" />
+      <meta property="og:description" content={description} />
+      <meta property="og:title" content={title} />
+    </Head>
+  );
+};
+
+export default DocumentHead;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,14 @@
 import Card from "@/components/Card";
+import DocumentHead from "@/components/DocumentHead";
 import TodoList from "@/containers/TodoList";
 
 export default function Home() {
   return (
     <Card>
+      <DocumentHead
+        title="Itemised"
+        description="Get organized and stay on top of your tasks with our web-based todo list application. Keep track of your to-do's, set deadlines, and prioritize your tasks with ease."
+      />
       <TodoList />
     </Card>
   );
