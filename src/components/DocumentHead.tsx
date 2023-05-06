1f9a0942e1d791160368541fb6b964d2ef423e82
Nermin <sehicnermin97@gmail.com>
2023-05-06 17:51:29 +1000

    fix open graph and meta

--- src/components/DocumentHead.tsx
+++ src/components/DocumentHead.tsx
@@ -9,8 +9,37 @@ interface Props {
 const DocumentHead: React.FC<Props> = ({ title, description }) => {
   return (
     <Head>
-      <title>{title}</title>
-      <meta name="description" content={description} />
+      <title>Itemised</title>
+      <meta
+        name="description"
+        content="Get organized and stay on top of your tasks with our web-based todo list application. Keep track of your to-do's, set deadlines, and prioritize your tasks with ease."
+      />
+
+      <meta property="og:url" content="https://itemised.vercel.app/" />
+      <meta property="og:type" content="website" />
+      <meta property="og:title" content="Itemised" />
+      <meta
+        property="og:description"
+        content="Get organized and stay on top of your tasks with our web-based todo list application. Keep track of your to-do's, set deadlines, and prioritize your tasks with ease."
+      />
+      <meta
+        property="og:image"
+        content="https://itemised.vercel.app/images/ogimage.png"
+      />
+
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta property="twitter:domain" content="itemised.vercel.app" />
+      <meta property="twitter:url" content="https://itemised.vercel.app/" />
+      <meta name="twitter:title" content="Itemised" />
+      <meta
+        name="twitter:description"
+        content="Get organized and stay on top of your tasks with our web-based todo list application. Keep track of your to-do's, set deadlines, and prioritize your tasks with ease."
+      />
+      <meta
+        name="twitter:image"
+        content="https://itemised.vercel.app/images/ogimage.png"
+      />
+
       <link
         rel="apple-touch-icon"
         sizes="180x180"
@@ -32,15 +61,6 @@ const DocumentHead: React.FC<Props> = ({ title, description }) => {
       <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#222222" />
       <meta name="msapplication-TileColor" content="#da532c" />
       <meta name="theme-color" content="#222222" />
-      <meta property="og:image" content="/images/ogimage.png" />
-      <meta property="og:image:type" content="image/png" />
-      <meta property="og:image:width" content="1200" />
-      <meta property="og:image:height" content="630" />
-      <meta property="og:image:alt" content="Itemised Logo" />
-      <meta property="og:type" content="website" />
-      <meta property="og:url" content="https://itemised.vercel.app" />
-      <meta property="og:description" content={description} />
-      <meta property="og:title" content={title} />
     </Head>
   );
 };
